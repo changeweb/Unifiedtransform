b2f37ae03d33604009e3ec55b5a7013479707f8d
Hasib <kishorpasha870@yahoo.com>
2019-03-19 14:35:59 +0600

    Fix migration table column
    Users table column department_id error fixed

--- database/migrations/2018_10_08_002853_add_department_class_teacher_to_users_table.php
+++ database/migrations/2018_10_08_002853_add_department_class_teacher_to_users_table.php
@@ -26,7 +26,7 @@ class AddDepartmentClassTeacherToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(['department']);
+            $table->dropColumn(['department_id']);
         });
     }
 }
