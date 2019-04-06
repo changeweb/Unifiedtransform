77bb24af47d4777a78b9364a205d6b2f2471c9d3
Hasib Mahmud <kishorpasha870@yahoo.com>
2019-04-06 20:43:01 +0600

    Improve messaging using CKeditor
    Now Rich text message can be sent using CKeditor

--- resources/views/course/students.blade.php
+++ resources/views/course/students.blade.php
@@ -3,6 +3,12 @@
 @section('title', 'Course Students')
 
 @section('content')
+<script src="https://cdn.ckeditor.com/ckeditor5/12.0.0/classic/ckeditor.js"></script>
+<style>
+.ck-editor__editable{
+    min-height: 200px;
+}
+</style>
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-2" id="side-navbar">
@@ -10,14 +16,15 @@
         </div>
         <div class="col-md-10" id="main-container">
             @if(count($students) > 0)
-                @foreach ($students as $student)
-                    <h3>Course Students of Class: {{$student->student->section->class->class_number}} Section: {{$student->student->section->section_number}}</h3>
-                    @break
-                @endforeach
-                <h4>Select Students to send message</h4>
+            @foreach ($students as $student)
+            <h3>Course Students of Class: {{$student->student->section->class->class_number}} Section:
+                {{$student->student->section->section_number}}</h3>
+            @break
+            @endforeach
+            <h4>Select Students to send message</h4>
             @endif
             <div class="panel panel-default">
-              @if(count($students) > 0)
+                @if(count($students) > 0)
                 <div class="panel-body">
                     <div class="col-md-6">
                         <table class="table table-bordered table-condensed table-striped">
@@ -37,31 +44,34 @@
                                 <td>
                                     <div class="checkbox">
                                         <label>
-                                            <input type="checkbox" name="recipients[]" form="msgForm" value="{{$student->student->id}}">
+                                            <input type="checkbox" name="recipients[]" form="msgForm"
+                                                value="{{$student->student->id}}">
                                         </label>
                                     </div>
                                 </td>
                                 <td>{{$student->student->student_code}}</td>
-                                <td><a href="{{url('user/'.$student->student->student_code)}}">{{$student->student->name}}</a></td>
+                                <td><a
+                                        href="{{url('user/'.$student->student->student_code)}}">{{$student->student->name}}</a>
+                                </td>
                             </tr>
                             @endforeach
                         </table>
                     </div>
                     <div class="col-md-6">
-                    @if (session('status'))
+                        @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
                         </div>
-                    @endif
-                    @if ($errors->any())
+                        @endif
+                        @if ($errors->any())
                         <div class="alert alert-danger">
                             <ul>
                                 @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
+                                <li>{{ $error }}</li>
                                 @endforeach
                             </ul>
                         </div>
-                    @endif
+                        @endif
                         <form action="{{url('message/students')}}" method="POST" id="msgForm">
                             {{ csrf_field() }}
                             <input type="hidden" name="teacher_id" value="{{$teacher_id}}">
@@ -70,27 +80,36 @@
                                 <label for="msg">Write Message: </label>
                                 <textarea name="msg" class="form-control" id="msg" cols="30" rows="10"></textarea>
                             </div>
-                            <button type="submit" class="btn btn-danger btn-sm"><i class="material-icons">send</i> Send Message</button>
+                            <button type="submit" class="btn btn-danger btn-sm"><i class="material-icons">send</i> Send
+                                Message</button>
                         </form>
                     </div>
                 </div>
                 <script>
-                    $(function(){
+                    $(function () {
                         var r = $(':checkbox[name="recipients[]"]');
-                        $('#selectAll').on('change',function(){
-                            if($(this).is(':checked')){
+                        $('#selectAll').on('change', function () {
+                            if ($(this).is(':checked')) {
                                 r.prop('checked', true);
                             } else {
                                 r.prop('checked', false);
                             }
                         });
+                        ClassicEditor
+                            .create(document.querySelector('#msg'), {
+                                toolbar: ['bold', 'italic','Heading', 'Link', 'bulletedList', 'numberedList', 'blockQuote']
+                            })
+                            .catch(error => {
+                                console.error(error);
+                            });
                     });
+
                 </script>
-              @else
+                @else
                 <div class="panel-body">
                     No Related Data Found.
                 </div>
-              @endif
+                @endif
             </div>
         </div>
     </div>
