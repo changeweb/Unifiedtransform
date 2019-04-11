ecfc3c6d5efea95d04267450c0f972a948bc87e5
Hasib Mahmud <kishorpasha870@yahoo.com>
2019-04-11 15:51:42 +0600

    Improve Notice list UI
    Add download icon and publish date time in notice, routine, event, syllabus list view.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
+    .badge-download {
+        background-color: transparent !important;
+        color: #464443 !important;
+    }
+    .list-group-item-text{
+      font-size: 12px;
+    }
+</style>
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-2" id="side-navbar">
@@ -10,137 +19,166 @@
             <div class="panel panel-default" style="border-top: 0px;">
                 <div class="panel-body">
                     @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success">
+                        {{ session('status') }}
+                    </div>
                     @endif
                     <div class="row">
-                      <div class="page-panel-title">Dashboard</div>
-                      <div class="col-sm-2">
-                        <div class="card text-white bg-primary mb-3">
-                          <div class="card-header">Total Students - <b>{{$totalStudents}}</b></div>
+                        <div class="page-panel-title">Dashboard</div>
+                        <div class="col-sm-2">
+                            <div class="card text-white bg-primary mb-3">
+                                <div class="card-header">Total Students - <b>{{$totalStudents}}</b></div>
+                            </div>
                         </div>
-                      </div>
-                      <div class="col-sm-2">
-                        <div class="card text-white bg-success mb-3">
-                          <div class="card-header">Total Teachers - <b>{{$totalTeachers}}</b></div>
+                        <div class="col-sm-2">
+                            <div class="card text-white bg-success mb-3">
+                                <div class="card-header">Total Teachers - <b>{{$totalTeachers}}</b></div>
+                            </div>
                         </div>
-                      </div>
-                      <div class="col-sm-4">
-                        <div class="card text-white bg-dark mb-3">
-                          <div class="card-header">Total Types of Books In Library - <b>{{$totalBooks}}</b></div>
+                        <div class="col-sm-4">
+                            <div class="card text-white bg-dark mb-3">
+                                <div class="card-header">Total Types of Books In Library - <b>{{$totalBooks}}</b></div>
+                            </div>
                         </div>
-                      </div>
-                      <div class="col-sm-2">
-                        <div class="card text-white bg-danger mb-3">
-                          <div class="card-header">Total Classes - <b>{{$totalClasses}}</b></div>
+                        <div class="col-sm-2">
+                            <div class="card text-white bg-danger mb-3">
+                                <div class="card-header">Total Classes - <b>{{$totalClasses}}</b></div>
+                            </div>
                         </div>
-                      </div>
-                      <div class="col-sm-2">
-                        <div class="card text-white bg-info mb-3">
-                          <div class="card-header">Total Sections - <b>{{$totalSections}}</b></div>
+                        <div class="col-sm-2">
+                            <div class="card text-white bg-info mb-3">
+                                <div class="card-header">Total Sections - <b>{{$totalSections}}</b></div>
+                            </div>
                         </div>
-                      </div>
                     </div>
                     <p></p>
                     <div class="row">
-                      <div class="col-sm-8">
-                        <div class="panel panel-default" style="background-color: rgba(242,245,245,0.8);">
-                          <div class="panel-body">
-                            <h3>Welcome to {{Auth::user()->school->name}}</h3>
-                            Your presence and cooperation will help us to improve the education system of our organization.
-                          </div>
+                        <div class="col-sm-8">
+                            <div class="panel panel-default" style="background-color: rgba(242,245,245,0.8);">
+                                <div class="panel-body">
+                                    <h3>Welcome to {{Auth::user()->school->name}}</h3>
+                                    Your presence and cooperation will help us to improve the education system of our
+                                    organization.
+                                </div>
+                            </div>
+                            <div class="panel panel-default">
+                                <div class="page-panel-title">Active Exams</div>
+                                <div class="panel-body">
+                                    @if(count($exams) > 0)
+                                    <table class="table">
+                                        <tr>
+                                            <th>Exam Name</th>
+                                            <th>Notice Published</th>
+                                            <th>Result Published</th>
+                                        </tr>
+                                        @foreach($exams as $exam)
+                                        <tr>
+                                            <td>{{$exam->exam_name}}</td>
+                                            <td>{{($exam->notice_published === 1)?'Yes':'No'}}</td>
+                                            <td>{{($exam->result_published === 1)?'Yes':'No'}}</td>
+                                        </tr>
+                                        @endforeach
+                                    </table>
+                                    @else
+                                    No Active Examination
+                                    @endif
+                                </div>
+                            </div>
                         </div>
-                        <div class="panel panel-default">
-                          <div class="page-panel-title">Active Exams</div>
-                          <div class="panel-body">
-                            @if(count($exams) > 0)
-                            <table class="table">
-                              <tr>
-                                <th>Exam Name</th>
-                                <th>Notice Published</th>
-                                <th>Result Published</th>
-                              </tr>
-                              @foreach($exams as $exam)
-                              <tr>
-                                <td>{{$exam->exam_name}}</td>
-                                <td>{{($exam->notice_published === 1)?'Yes':'No'}}</td>
-                                <td>{{($exam->result_published === 1)?'Yes':'No'}}</td>
-                              </tr>
-                              @endforeach
-                            </table>
-                            @else
-                              No Active Examination
-                            @endif
-                          </div>
+                        <div class="col-sm-4">
+                            <div class="panel panel-default">
+                                <div class="page-panel-title">Notices</div>
+                                <div class="panel-body pre-scrollable">
+                                    @if(count($notices) > 0)
+                                    <div class="list-group">
+                                        @foreach($notices as $notice)
+                                        <a href="{{url($notice->file_path)}}" class="list-group-item" download>
+                                            <i class="badge badge-download material-icons">
+                                                get_app
+                                            </i>
+                                            <h5 class="list-group-item-heading">{{$notice->title}}</h5>
+                                            <p class="list-group-item-text">Published at:
+                                                {{$notice->created_at->format('M d Y h:i:sa')}}</p>
+                                        </a>
+                                        @endforeach
+                                    </div>
+                                    @else
+                                    No New Notice
+                                    @endif
+                                </div>
+                            </div>
                         </div>
-                      </div>
-                      <div class="col-sm-4">
-                        <div class="panel panel-default">
-                          <div class="page-panel-title">Notices</div>
-                          <div class="panel-body pre-scrollable">
-                            @if(count($notices) > 0)
-                              <div class="list-group">
-                              @foreach($notices as $notice)
-                                <a href="{{url($notice->file_path)}}" class="list-group-item" download>{{$notice->title}}</a>
-                              @endforeach
-                              </div>
-                            @else
-                              No New Notice
-                            @endif
-                          </div>
-                        </div>
-                      </div>
                     </div>
                     <div class="row">
-                      <div class="col-sm-4">
-                        <div class="panel panel-default">
-                          <div class="page-panel-title">Events</div>
-                          <div class="panel-body pre-scrollable">
-                            @if(count($events) > 0)
-                              <div class="list-group">
-                              @foreach($events as $event)
-                                <a href="{{url($event->file_path)}}" class="list-group-item" download>{{$event->title}}</a>
-                              @endforeach
-                              </div>
-                            @else
-                              No New Event
-                            @endif
-                          </div>
+                        <div class="col-sm-4">
+                            <div class="panel panel-default">
+                                <div class="page-panel-title">Events</div>
+                                <div class="panel-body pre-scrollable">
+                                    @if(count($events) > 0)
+                                    <div class="list-group">
+                                        @foreach($events as $event)
+                                        <a href="{{url($event->file_path)}}" class="list-group-item" download>
+                                            <i class="badge badge-download material-icons">
+                                                get_app
+                                            </i>
+                                            <h5 class="list-group-item-heading">{{$event->title}}</h5>
+                                            <p class="list-group-item-text">Published at:
+                                                {{$event->created_at->format('M d Y')}}</p>
+                                        </a>
+                                        @endforeach
+                                    </div>
+                                    @else
+                                    No New Event
+                                    @endif
+                                </div>
+                            </div>
                         </div>
-                      </div>
-                      <div class="col-sm-4">
-                        <div class="panel panel-default">
-                          <div class="page-panel-title">Routines</div>
-                          <div class="panel-body pre-scrollable">
-                            @if(count($routines) > 0)
-                              <div class="list-group">
-                              @foreach($routines as $routine)
-                                <a href="{{url($routine->file_path)}}" class="list-group-item" download>{{$routine->title}}</a>
-                              @endforeach
-                              </div>
-                            @else
-                              No New Routine
-                            @endif
-                          </div>
+                        <div class="col-sm-4">
+                            <div class="panel panel-default">
+                                <div class="page-panel-title">Routines</div>
+                                <div class="panel-body pre-scrollable">
+                                    @if(count($routines) > 0)
+                                    <div class="list-group">
+                                        @foreach($routines as $routine)
+                                        <a href="{{url($routine->file_path)}}" class="list-group-item" download>
+                                            <i class="badge badge-download material-icons">
+                                                get_app
+                                            </i>
+                                            <h5 class="list-group-item-heading">{{$routine->title}}</h5>
+                                            <p class="list-group-item-text">Published at:
+                                                {{$routine->created_at->format('M d Y')}}</p>
+                                        </a>
+                                        @endforeach
+                                    </div>
+                                    @else
+                                    No New Routine
+                                    @endif
+                                </div>
+                            </div>
                         </div>
-                      </div>
-                      <div class="col-sm-4">
-                        <div class="panel panel-default">
-                          <div class="page-panel-title">Syllabus</div>
-                          <div class="panel-body pre-scrollable">
-                            @if(count($syllabuses) > 0)
-                              <div class="list-group">
-                                @foreach($syllabuses as $syllabus)
-                                <a href="{{url($syllabus->file_path)}}" class="list-group-item" download>{{$syllabus->title}}</a>
-                                @endforeach
-                              </div>
-                            @else
-                              No New Syllabus
-                            @endif
-                          </div>
+                        <div class="col-sm-4">
+                            <div class="panel panel-default">
+                                <div class="page-panel-title">Syllabus</div>
+                                <div class="panel-body pre-scrollable">
+                                    @if(count($syllabuses) > 0)
+                                    <div class="list-group">
+                                        @foreach($syllabuses as $syllabus)
+                                        <a href="{{url($syllabus->file_path)}}" class="list-group-item" download>
+                                            <i class="badge badge-download material-icons">
+                                                get_app
+                                            </i>
+                                            <h5 class="list-group-item-heading">{{$syllabus->title}}</h5>
+                                            <p class="list-group-item-text">Published at:
+                                                {{$syllabus->created_at->format('M d Y')}}</p>
+                                        </a>
+                                        @endforeach
+                                    </div>
+                                    @else
+                                    No New Syllabus
+                                    @endif
+                                </div>
+                            </div>
                         </div>
-                      </div>
                     </div>
                 </div>
             </div>
