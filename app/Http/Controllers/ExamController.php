8dd2ce1f8926ba5f1e2815e389c9a04cc7f05114
Hasib Mahmud <kishorpasha870@yahoo.com>
2019-05-01 20:31:30 +0600

    Add ExamService.php
    Update ExamController.php, Add CreateExamRequest.php

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\Exam\ExamService;
+use App\Http\Requests\Exam\CreateExamRequest;
 
 class ExamController extends Controller
 {
+    protected $examService;
+
+    public function __construct(ExamService $examService){
+        $this->examService = $examService;
+    }
     /**
      * Display a listing of the resource.
      *
@@ -13,28 +20,16 @@ class ExamController extends Controller
      */
     public function index()
     {
-        $exams = \App\Exam::where('school_id', \Auth::user()->school_id)
-                    ->latest()
-                    ->paginate(100);
-        return view('exams.all',['exams'=>$exams]);
+        $exams = $this->examService->getLatestExamsBySchoolIdWithPagination();
+        return view('exams.all',compact('exams'));
     }
 
     public function indexActive(){
-        $examIds = \App\Exam::where('school_id', \Auth::user()->school_id)
-                    ->where('active',1)
-                    ->pluck('id')
-                    ->toArray();
-        $exams = \App\Exam::where('school_id', \Auth::user()->school_id)
-                    ->where('active',1)
-                    ->get();
-        $courses = \App\Course::with('class','teacher')
-                    ->whereIn('exam_id', $examIds)
-                    ->orderBy('class_id')
-                    ->get();
-        return view('exams.active',[
-            'exams'=>$exams,
-            'courses'=>$courses
-        ]);
+        $exams = $this->examService->getActiveExamsBySchoolId();
+        $this->examService->examIds = $exams->pluck('id')->toArray();
+        $courses = $this->examService->getCoursesByExamIds();
+
+        return view('exams.active',compact('exams','courses'));
     }
 
     /**
@@ -44,8 +39,8 @@ class ExamController extends Controller
      */
     public function create()
     {
-        $classes = \App\Myclass::where('school_id',\Auth::user()->school->id)->get();
-        return view('exams.add',['classes'=>$classes]);
+        $classes = $this->examService->getClassesBySchoolId();
+        return view('exams.add',compact('classes'));
     }
 
     /**
@@ -54,45 +49,15 @@ class ExamController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CreateExamRequest $request)
     {
-        $request->validate([
-            'exam_name' => 'required|string',
-            'term' => 'required|string',
-            'start_date' => 'required|string',
-            'end_date' => 'required|string',
-        ]);
-
-        \DB::transaction(function () use ($request) {
-            $exam = new \App\Exam;
-            $exam->exam_name = $request->exam_name;
-            $exam->active = 1;
-            $exam->term = $request->term;
-            $exam->start_date = $request->start_date;
-            $exam->end_date = $request->end_date;
-            $exam->notice_published = 0;
-            $exam->result_published = 0;
-            $exam->school_id = \Auth::user()->school_id;
-            $exam->user_id = \Auth::user()->id;
-            $exam->save();
-            $i = 0;
-            $tc = count($request->classes);
         
-            // Assign Exam ID to Classes in Course Table
-            \App\Course::whereIn('class_id',$request->classes)->update([
-                'exam_id' => $exam->id
-            ]);
-
-            while($i < $tc){
-                $examForClass = new \App\ExamForClass;
-                $examForClass->exam_id = $exam->id;
-                $examForClass->class_id = $request->classes[$i];
-                $efc[] = $examForClass->attributesToArray();
-                $i++;
-            }
-            if(count($efc) > 0)
-                \App\ExamForClass::insert($efc);
-        }, 5);
+        $this->examService->request = $request;
+        try{
+            $this->examService->storeExam();
+        } catch (\Exception $e){
+            return 'Error: '. $e->getMessage();
+        }
         
         //return $this->cindex($course_id, $exam_id, $teacher_id);
         return back()->with('status', 'Created');
@@ -132,16 +97,12 @@ class ExamController extends Controller
         $request->validate([
             'exam_id' => 'required|numeric',
         ]);
-        \DB::transaction(function () use ($request) {
-            $tb = \App\Exam::find($request->exam_id);
-            $tb->notice_published = isset($request->notice_published)?1:0;
-            $tb->result_published = isset($request->result_published)?1:0;
-            $tb->active = (isset($request->active))?1:0;
-            $tb->save();
-            if(!isset($request->active)){
-                \App\ExamForClass::where('exam_id', $request->exam_id)->update(['active'=>0]);
-            }
-        });
+        try{
+            $this->examService->request = $request;
+            $this->examService->updateExam();
+        } catch (\Exception $e){
+            return 'Error: '. $e->getMessage();
+        }
         return back()->with('status', 'Saved');
     }
 
