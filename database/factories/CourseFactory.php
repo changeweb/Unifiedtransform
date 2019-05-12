157ab4b1f1096256bd2f3927686f14f2bdccca4b
Juan Vásquez <juanthoniovg@gmail.com>
2019-05-12 15:23:35 -0500

    add changes factories

--- database/factories/CourseFactory.php
+++ database/factories/CourseFactory.php
@@ -1,77 +1,68 @@
 <?php
 
+use App\User;
+use App\Exam;
+use App\School;
+use App\Course;
+use App\Myclass;
+use App\Section;
+use App\Gradesystem;
 use Faker\Generator as Faker;
 
-$factory->define(App\Course::class, function (Faker $faker) {
+$factory->define(Course::class, function (Faker $faker) {
     return [
         'course_name' => $faker->words(3, true),
-        'class_id' => function () use ($faker) {
-          if(App\Myclass::count() == 0)
-            return factory(App\Myclass::class)->create()->id;
-          else {
-            return $faker->randomElement(App\Myclass::pluck('id')->toArray());
-          }
+        'class_id'    => function () use ($faker) {
+            if (Myclass::count())
+                return $faker->randomElement(Myclass::pluck('id')->toArray());
+            else return factory(Myclass::class)->create()->id;
         },
         'course_type' => $faker->randomElement(['Core','Elective']),
         'course_time' => $faker->randomElement(['9:30AM-10:20AM','12:50PM-01:40PM']),
-        'school_id' => function () use ($faker) {
-          if(App\School::count() == 0)
-            return factory(App\School::class)->create()->id;
-          else {
-            return $faker->randomElement(App\School::pluck('id')->toArray());
-          }
+        'school_id'   => function () use ($faker) {
+            if (School::count())
+                return $faker->randomElement(School::pluck('id')->toArray());
+            else return factory(School::class)->create()->id;
         },
-        'teacher_id' => function () use ($faker) {
-          if(App\User::where('role', 'teacher')->count() == 0)
-            return factory(App\User::class)->create([
-                    'role' => 'teacher',
-            ])->id;
-          else {
-            return $faker->randomElement(App\User::where('role', 'teacher')->take(10)->pluck('id')->toArray());
-          }
+        'teacher_id'  => function () use ($faker) {
+            if (User::where('role', 'teacher')->count())
+                return $faker->randomElement(User::where('role', 'teacher')->take(10)->pluck('id')->toArray());
+            else return factory(User::class)->create(['role' => 'teacher'])->id;
         },
-        'section_id' => function () use ($faker) {
-          if(App\Section::count() == 0)
-            return factory(App\Section::class)->create()->id;
-          else {
-            return $faker->randomElement(App\Section::pluck('id')->toArray());
-          }
+        'section_id'        => function () use ($faker) {
+            if (Section::count())
+                return $faker->randomElement(Section::pluck('id')->toArray());
+            else return factory(Section::class)->create()->id;
         },
         'grade_system_name' => function () use ($faker) {
-          if(App\Gradesystem::count() == 0)
-            return factory(App\Gradesystem::class)->create()->grade_system_name;
-          else {
-            return $faker->randomElement(App\Gradesystem::pluck('grade_system_name')->toArray());
-          }
+            if (Gradesystem::count())
+                return $faker->randomElement(Gradesystem::pluck('grade_system_name')->toArray());
+            else return factory(Gradesystem::class)->create()->grade_system_name;
         },
-        'exam_id' => function () use ($faker) {
-          if(App\Exam::count() == 0)
-            return factory(App\Exam::class)->create()->id;
-          else {
-            return $faker->randomElement(App\Exam::pluck('id')->toArray());
-          }
+        'exam_id'           => function () use ($faker) {
+            if (Exam::count())
+                return $faker->randomElement(Exam::pluck('id')->toArray());
+            else return factory(Exam::class)->create()->id;
         },
-        'quiz_count'=>$faker->randomElement([1,2,3,4,5]),
-        'assignment_count'=>$faker->randomElement([1,2,3]),
-        'ct_count'=>$faker->randomElement([1,2,3,4,5]),
-        'quiz_percent' => 10,
+        'quiz_count'         => $faker->randomElement([1,2,3,4,5]),
+        'assignment_count'   => $faker->randomElement([1,2,3]),
+        'ct_count'           => $faker->randomElement([1,2,3,4,5]),
+        'quiz_percent'       => 10,
         'attendance_percent' => 5,
         'assignment_percent' => 15,
-        'ct_percent' => 10,
+        'ct_percent'         => 10,
         'final_exam_percent' => 50,
-        'practical_percent' => 25,
-        'att_fullmark' => 5,
-        'quiz_fullmark' => 15,
-        'a_fullmark' => 20,
-        'ct_fullmark' => 15,
-        'final_fullmark' => 100,
+        'practical_percent'  => 25,
+        'att_fullmark'       => 5,
+        'quiz_fullmark'      => 15,
+        'a_fullmark'         => 20,
+        'ct_fullmark'        => 15,
+        'final_fullmark'     => 100,
         'practical_fullmark' => 30,
         'user_id' => function () use ($faker) {
-          if(App\User::count() == 0)
-            return factory(App\User::class)->create()->id;
-          else {
-            return $faker->randomElement(App\User::pluck('id')->toArray());
-          }
+            if (User::count())
+                return $faker->randomElement(User::pluck('id')->toArray());
+            else return factory(User::class)->create()->id;
         },
     ];
 });
--- database/factories/FeeFactory.php
+++ database/factories/FeeFactory.php
@@ -1,36 +1,36 @@
 <?php
 
+use App\Fee;
+use App\User;
+use App\School;
 use Faker\Generator as Faker;
 
-$factory->define(App\Fee::class, function (Faker $faker) {
+$factory->define(Fee::class, function (Faker $faker) {
     return [
-      'fee_name' => $faker->name,
-      'school_id' => function () use ($faker) {
-          if(App\School::count() == 0)
-            return factory(App\School::class)->create()->id;
-          else {
-            return $faker->randomElement(App\School::pluck('id')->toArray());
-          }
+        'fee_name'  => $faker->name,
+        'school_id' => function() use ($faker) {
+            if (School::count())
+                return $faker->randomElement(School::pluck('id')->toArray());
+            else return factory(School::class)->create()->id;
         },
-      // 'admission_fee' => $faker->randomNumber,
-      // 'tution_fee' => $faker->randomNumber,
-      // 'fine_fee' => $faker->randomNumber,
-      // 'exam_fee' => $faker->randomNumber,
-      // 'registration_fee' => $faker->randomNumber,
-      // 'library_fee' => $faker->randomNumber,
-      // 'lab_fee' => $faker->randomNumber,
-      // 'sport_fee' => $faker->randomNumber,
-      // 'late_payment_fee' => $faker->randomNumber,
-      // 'maintenance_fee' => $faker->randomNumber,
-      // 'internet_fee' => $faker->randomNumber,
-      // 'farewell_fee' => $faker->randomNumber,
-      // 'other_fee' => $faker->randomNumber,
-      'user_id' => function () use ($faker) {
-          if(App\User::count() == 0)
-            return factory(App\User::class)->create()->id;
-          else {
-            return $faker->randomElement(App\User::pluck('id')->toArray());
-          }
+        'user_id'   => function() use ($faker) {
+            if (User::count())
+                return $faker->randomElement(User::pluck('id')->toArray());
+            else return factory(User::class)->create()->id;
         },
     ];
 });
+
+// 'admission_fee'    => $faker->randomNumber,
+// 'tution_fee'       => $faker->randomNumber,
+// 'fine_fee'         => $faker->randomNumber,
+// 'exam_fee'         => $faker->randomNumber,
+// 'registration_fee' => $faker->randomNumber,
+// 'library_fee'      => $faker->randomNumber,
+// 'lab_fee'          => $faker->randomNumber,
+// 'sport_fee'        => $faker->randomNumber,
+// 'late_payment_fee' => $faker->randomNumber,
+// 'maintenance_fee'  => $faker->randomNumber,
+// 'internet_fee'     => $faker->randomNumber,
+// 'farewell_fee'     => $faker->randomNumber,
+// 'other_fee'        => $faker->randomNumber,
