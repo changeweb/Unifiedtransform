b782fe31a8269110c7b3a05c30668fc1a6510070
Juan Antonio <juanthoniovg@gmail.com>
2019-07-09 22:49:32 -0500

    Now master user is redirect to /masters view when sign in

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -2,34 +2,26 @@
 
 namespace Tests\Browser;
 
+use App\User;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class LoginTest extends DuskTestCase
 {
-    
-    public function tearDown()
-    {
-        session()->flush();
+    use DatabaseMigrations;
 
-        parent::tearDown();
-    }
-    /**
-     * A Dusk test login.
-     *
-     * @return void
-     */
-    public function testLogin()
-    {
-        $user = \App\User::where('role','master')->first();
+    /** @test */
+    public function user_master_can_sign_in() {
+        $user = factory(User::class)->states('master')->create();
 
         $this->browse(function ($browser) use ($user) {
             $browser->visit('/login')
-                    ->type('email', $user['email'])
+                    ->waitForText('Login')
+                    ->type('email', $user->email)
                     ->type('password', 'secret')
                     ->press('Login')
-                    ->assertPathIs('/home');
+                    ->assertPathIs('/masters');
         });
     }
 }
